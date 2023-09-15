3b913bcb101fab75304f033189243390168db582
Sea-queue <chengqian1214@gmail.com>
2023-09-15 13:27:50 -0400

    upgraded the login sign in views, and the header

--- app/models/gnt_user.rb
+++ app/models/gnt_user.rb
@@ -3,6 +3,12 @@ class GntUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # enum role: [:candidate, :employer, :recruiter, :admin]
+  # after_initialize :set_default_role, :if => :new_record?
+  # def set_default_role
+  #   self.role ||= :candidate
+  # end
 
   def admin?
     role == "admin"
@@ -10,6 +16,10 @@ class GntUser < ApplicationRecord
 
   def candidate?
     role == 'candidate'
+  end
+
+  def employer?
+    role == 'employer'
   end
   # validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 end
