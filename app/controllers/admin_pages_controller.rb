4e0428fbf84d3baf90af6893d27d58b86c97cb1e
Sea-queue <chengqian1214@gmail.com>
2023-10-25 19:07:25 -0400

    changed filtering to params only, instead of sessions + params, filter layering is working now, need more tests and update other filters

--- app/controllers/admin_pages_controller.rb
+++ app/controllers/admin_pages_controller.rb
@@ -9,27 +9,34 @@ class AdminPagesController < ApplicationController
   private
   def initialize_search
     @users = GntUser.where(role: "candidate")
-    session[:search_name] ||= params[:search_name]
-    session[:filter] = params[:filter]
-    params[:filter_option] = nil if params[:filter_option] == ""
-    session[:filter_option] = params[:filter_option]
   end
 
   def handle_search_name
-    session[:search_name] = params[:search_name]
-    if session[:search_name]
-      @users = GntUser.where(legal_name: session[:search_name])
-    else
-      @users = GntUser.where(role: "candidate")
+    if params[:search_name] and params[:search_name] != ""
+      @users = GntUser.where(legal_name: params[:search_name])
     end
   end
 
   def handle_filters
-    if session[:filter_option] && session[:filter] == "nationality"
-      @users = GntUser.all.where(nationality: session[:filter_option])
-    elsif session[:filter_option] && session[:filter] == "languages_spoken"
-      puts("filter:", session[:filter_option])
-      @users = @users.where(languages_spoken: session[:filter_option])
+    if params[:nationality_filter] and params[:nationality_filter] != ""
+      puts('nationality_filter', params[:nationality_filter])
+      @users = @users.where(nationality: params[:nationality_filter])
+    end
+    if params[:experience_years_filter] and params[:experience_years_filter] != ""
+      puts('experience_years_filter', params[:experience_years_filter])
+      @users = @users.where(years_of_experience: ['3', '4', '5+'])
+    end
+    if session[:english_proficiency_filter]
+      puts('english_proficiency_filter')
+      @users = @users.where(english_proficiency: session[:english_proficiency_filter])
+    end
+    if params[:nclex_filter] and params[:nclex_filter] != ""
+      puts('nclex_filter', params[:nclex_filter])
+      @users = @users.where(nclex_rn: params[:nclex_filter])
+    end
+    if session[:languages_spoken_filter]
+      puts('languages_spoken_filter')
+      @users = @users.where(languages_spoken: session[:languages_spoken_filter])
     end
   end
 end
